4f7c3e681dece7599fbf44854242339b3fc30b4d
redazures <jwu_nyc@yahoo.com>
2020-09-08 22:30:26 -0400

    prices are fucked

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -5,8 +5,27 @@ class CurrenciesController < ApplicationController
         render json: currency
     end
 
+    def show
+        currency=Currency.find_by(id: params[:id])
+        if currency
+            # render json: member.to_json(include: [:transactions, :currencies])
+            render json: currency.to_json
+          else
+            render json: { message: 'member not found' }
+        end
+    end
+
+    def update
+        currency=Currency.find_by(id: params[:id])
+        currency=Currency.update(currency_params)
+        render json: currency
+    end
+
     # def show
     #     currency = Currency.all
     #     render json: currency
     # end
+    def currency_params
+        params.require(:currency).permit(:price,:symbol,:name)
+    end
 end
